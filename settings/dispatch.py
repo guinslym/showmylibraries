01ebd69bc761f26809e571a3c6aa5e0491e01fe8
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-14 00:08:15 -0400

    warning

--- settings/dispatch.py
+++ settings/dispatch.py
@@ -5,6 +5,9 @@ if 'guinsly' in socket.gethostname():
     from .development import *
     print('--dev--settings--')
 else:
+    # if you cloned this app
+    # rename the file settings/production_py_example to
+    # production.py
     from .production import *
     #print('prod--settings')
 #this file won't be load in git and in the
